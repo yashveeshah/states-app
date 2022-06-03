ec029c0a245a9efa13da3ec1bc2ff90d8a814f83
yashveeshah <86735703+yashveeshah@users.noreply.github.com>
2022-06-03 09:34:52 -0700

    Add files via upload

---
+++ database.js
@@ -0,0 +1,304 @@
+const Information = {
+  "Alabama" : {
+    "capital" : "Montgomery",
+    "state flower":"Camellia",
+    "state bird" : "Northern Flicker",
+    "population":"4.9 million"
+  },
+  "Alaska" : {
+    "capital" : "Juneau",
+    "state flower":"Alpine Forget me-not",
+    "state bird" :"Willow Ptarmigan",
+    "population":"736,990"
+  },
+  "Arizona" : {
+    "capital" : "Phoenix",
+    "state flower":"Saguaro Cactus Blossom",
+    "state bird" : "Cactus Wren",
+    "population":"7.2 million"
+  },
+  "Arkansas" : {
+    "capital" : "Little Rock",
+    "state flower":"Apple Blossom",
+    "state bird" : "Mockingbird",
+    "population":"3.012 million"
+  },
+  "California" : {
+    "capital" : "Sacramento",
+    "state flower":"California Poppy",
+    "state bird" : "California Quail",
+    "population":"39.35 million"
+  },
+  "Colorado" : {
+    "capital" : "Denver",
+    "state flower":"Rocky Mountain Columbine",
+    "state bird" : "Lark Bunting",
+    "population":"5.7 million"
+  },
+  "Connecticut" : {
+    "capital" : "Hartford",
+    "state flower":"Mountain Laurel",
+    "state bird" : "American Robin",
+    "population":"3.6 million"
+  },
+  "Delaware" : {
+    "capital" : "Dover",
+    "state flower":"Peach Blossom",
+    "state bird" : "Blue Hen Chicken",
+    "population":"968,000"
+  },
+  "Georgia" : {
+    "capital" : "Atlanta",
+    "state flower":"Cherokee Rose",
+    "state bird" : "Brown Thrasher",
+    "population":"10.52 million"
+  },
+  "Florida" : {
+    "capital" : "Tallahassee",
+    "state flower":"Orange Blossom",
+    "state bird" : "Mockingbird",
+    "population":"21.22 million"
+  },
+  "Idaho" : {
+    "capital" : "Boise",
+    "state flower":"Syringa",
+    "state bird" : "Peregrine Falcon",
+    "population":"1.75 million"
+  },
+  "Hawaii" : {
+    "capital" : "Honolulu",
+    "state flower":"Lokelani",
+    "state bird" : "Nene",
+    "population":"1.42 million"
+  },
+  "Illinois" : {
+    "capital" : "Springfield",
+    "state flower":"Violet",
+    "state bird" : "Northern Cardinal",
+    "population":"12.72 million"
+  },
+  "Iowa" : {
+    "capital" : "Des Moines",
+    "state flower":"Wild Rose",
+    "state bird" : "Eastern Goldfinch",
+    "population":"3.15 million"
+  },
+  "Indiana" : {
+    "capital" : "Indianapolis",
+    "state flower":"Peony",
+    "state bird" : "Northern Cardinal",
+    "population":"6.7 million"
+  },
+  "Kansas" : {
+    "capital" : "Topeka",
+    "state flower":"Wild Native Sunflower",
+    "state bird" : "Western Meadowlark",
+    "population":"2.9 million"
+  },
+  "Kentucky" : {
+    "capital" : "Frankfort",
+    "state flower":"Goldenrod",
+    "state bird" : "Northern Cardinal",
+    "population":"4.5 million"
+  },
+  "Louisiana" : {
+    "capital" : "Baton Rouge",
+    "state flower":"Lousiana Iris",
+    "state bird" : "Brown Pelican",
+    "population":"4.7 million"
+  },
+  "Maine" : {
+    "capital" : "Augusta",
+    "state flower":"White Pine Cone and Tassel",
+    "state bird" : "Black-capped Chickadee",
+    "population":"1.34 million"
+  },
+  "Maryland" : {
+    "capital" : "Annapolis",
+    "state flower":"Black-Eyed Susan",
+    "state bird" : "Baltimore Oriole",
+    "population":"6.04 million"
+  },
+  "Massachusetts" : {
+    "capital" : "Boston",
+    "state flower":"Mayflower",
+    "state bird" : "Black-Capped Chickadee",
+    "population":"6.9 million"
+  },
+  "Michigan" : {
+    "capital" : "Lansing",
+    "state flower":"Apple Blossom",
+    "state bird" : "Robin Redbreast",
+    "population":"9.97 million"
+  },
+  "Minnesota" : {
+    "capital" : "Saint Paul",
+    "state flower":"Pink and White Lady Slipper",
+    "state bird" : "Common Loon",
+    "population":"5.6 million"
+  },
+  "Mississippi" : {
+    "capital" : "Jackson",
+    "state flower":"Magnolia",
+    "state bird" : "Wood Duck",
+    "population":"2.98 million"
+  },
+  "Missouri" : {
+    "capital" : "Jefferson City",
+    "state flower":"Coreopsis",
+    "state bird" : "Eastern Bluebird",
+    "population":"6.124 million"
+  },
+  "Montana" : {
+    "capital" : "Helena",
+    "state flower":"Bitterroot",
+    "state bird" : "Western Meadowlark",
+    "population":"1.062 million"
+  },
+  "Nebraska" : {
+    "capital" : "Lincoln",
+    "state flower":"Goldenrod",
+    "state bird" : "Western Meadowlark",
+    "population":"1.924 million"
+  },
+  "Nevada" : {
+    "capital" : "Carson City",
+    "state flower":"Sagebrush",
+    "state bird" : "Mountain Bluebird",
+    "population":"3.03 million"
+  },
+  "New Hampshire" : {
+    "capital" : "Concord",
+    "state flower":"Purple Lilac",
+    "state bird" : "Purple Finch",
+    "population":"1.355 million"
+  },
+  "New Jersey" : {
+    "capital" : "Trenton",
+    "state flower":"Violet",
+    "state bird" : "Eastern Goldfinch",
+    "population":"8.885 million"
+  },
+  "New Mexico" : {
+    "capital" : "Santa Fe",
+    "state flower":"Yucca",
+    "state bird" : "Greater Roadrunner",
+    "population":"2.097 million"
+  },
+  "New York" : {
+    "capital" : "Albany",
+    "state flower":"Rose",
+    "state bird" : "Eastern Bluebird",
+    "population":"8.38 million"
+  },
+  "North Carolina" : {
+    "capital" : "Raleigh",
+    "state flower":"Dogwood",
+    "state bird" : "Northern Cardinal",
+    "population":"10.39 million"
+  },
+  "North Dakota" : {
+    "capital" : "Bismarck",
+    "state flower":"White Trillium",
+    "state bird" : "Western Meadowlark",
+    "population":"760,394"
+  },
+  "Ohio" : {
+    "capital" : "Columbus",
+    "state flower":"Red Carnation",
+    "state bird" : "Northern Cardinal",
+    "population":"11.68 million"
+  },
+  "Oklahoma" : {
+    "capital" : "Oklahoma City",
+    "state flower":"Mistletoe",
+    "state bird" : "Scissor-tailed Flycatcher",
+    "population":"3.95 million"
+  },
+  "Oregon" : {
+    "capital" : "Salem",
+    "state flower":"Oregon Grape",
+    "state bird" : "Western Meadowlark",
+    "population":"4.176 million"
+  },
+  "Pennsylvania" : {
+    "capital" : "Harrisburg",
+    "state flower":"Mountain Laurel",
+    "state bird" : "Ruffed Grouse",
+    "population":"12.79 million"
+  },
+  "Rhode Island" : {
+    "capital" : "Providence",
+    "state flower":"Violet",
+    "state bird" : "Rhode Island Red",
+    "population":"1.058 million"
+  },
+  "South Carolina" : {
+    "capital" : "Columbia",
+    "state flower":"Yellow Jessamine",
+    "state bird" : "Wild Turkey",
+    "population":"5.1 million"
+  },
+  "South Dakota" : {
+    "capital" : "Pierre",
+    "state flower":"American Pasque",
+    "state bird" : "Ring-Necked Pheasant",
+    "population":"879,336"
+  },
+  "Tennessee" : {
+    "capital" : "Nashville",
+    "state flower":"Tennessee Coneflower",
+    "state bird" : "Mockingbird",
+    "population":"6.772 million"
+  },
+  "Texas" : {
+    "capital" : "Austin",
+    "state flower":"Bluebonnet",
+    "state bird" : "Mockingbird",
+    "population":"28.64 million"
+  },
+  "Utah" : {
+    "capital" : "Salt Lake City",
+    "state flower":"Sego Lily",
+    "state bird" : "California Gull",
+    "population":"3.151 million"
+  },
+  "Vermont" : {
+    "capital" : "Montpelier",
+    "state flower":"Red Clover",
+    "state bird" : "Hermit Thrush",
+    "population":"624,340"
+  },
+  "Virginia" : {
+    "capital" : "Richmond",
+    "state flower":"American Dogwood",
+    "state bird" : "Northern Cardinal",
+    "population":"8.51 million"
+  },
+  "Washington" : {
+    "capital" : "Olympia",
+    "state flower":"Coast Rhododendron",
+    "state bird" : "Willow Goldfinch",
+    "population":"7.512 million"
+  },
+  "West Virginia" : {
+    "capital" : "Charleston",
+    "state flower":"Rhododendron",
+    "state bird" : "Northern Cardinal",
+    "population":"1.807 million"
+  },
+  "Wisconsin" : {
+    "capital" : "Madison",
+    "state flower":"Wood Violet",
+    "state bird" : "Mourning Dove",
+    "population":"5.807 million"
+  },
+  "Wyoming" : {
+    "capital" : "Cheyenne",
+    "state flower":"Indian Paintbrush",
+    "state bird" : "Western Meadowlark",
+    "population":"581,348"
+  },
+}
+
+export default dictionary
